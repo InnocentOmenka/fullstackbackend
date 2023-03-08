1f57b9d8d88c2270f72a1bb6e01e279722c6bd2a
InnocentOmenka <innocent.omenka@decagon.dev>
2023-03-08 14:38:19 +0100

    docker file added

--- src/main/java/com/inno/fullstack/backend/model/User.java
+++ src/main/java/com/inno/fullstack/backend/model/User.java
@@ -4,8 +4,6 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 
 
 @Entity
